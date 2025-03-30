05b9396f4866f44bb979a3a31482cefb5eafe95c
PARTHO MANDOL <105819824+parthobd1@users.noreply.github.com>
2025-03-30 23:53:24 +0600

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,21 @@
+<?php include_once('header.php') ?>
+
+<section>
+    <h1>Task3 : CSSGrid</h1>
+    <div class="image-gallery-area">
+        <div class="container">
+            <div class="image-gallery-wrapper">
+                <div class="gallery">
+                    <img src="https://img3.wallspic.com/crops/8/1/2/1/71218/71218-ocean-promontory-coast-sea-caribbean-3840x2160.jpg" alt="">
+                    <img src="https://e0.pxfuel.com/wallpapers/683/15/desktop-wallpaper-maldives-background.jpg" alt="">
+                    <img src="https://rare-gallery.com/thumbs/576693-beach-backgrounds.jpeg" alt="">
+                    <img src="https://getwallpapers.com/wallpaper/full/d/3/6/1457021-cool-tropical-desktop-wallpaper-1920x1080-for-phones.jpg" alt="">
+                    <img src="https://e0.pxfuel.com/wallpapers/318/815/desktop-wallpaper-tropical-resort-px.jpg" alt="">
+                    <img src="https://e0.pxfuel.com/wallpapers/431/295/desktop-wallpaper-relaxing-full-background-relaxing-portrait.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once('footer.php') ?>

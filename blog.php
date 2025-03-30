6ecb3861a2297f49d6ea4dde24d1f925ba319e48
PARTHO MANDOL <105819824+parthobd1@users.noreply.github.com>
2025-03-31 00:00:00 +0600

    Delete blog.php

--- blog.php
+++
@@ -1,9 +0,0 @@
-<?php include_once('header.php') ?>
- 
-<section class="hero" id="hero">
-    <div class="hero-text">
-    <h1>Blog</h1>
-    <p>Will be updated soon.</p>
-    </div>
-</section>
-<?php include_once('footer.php') ?>

e5114ee9fb0fa64f6770085bd0bb58c233748c43
PARTHO MANDOL <105819824+parthobd1@users.noreply.github.com>
2025-03-31 00:01:00 +0600

    Delete services.php

--- services.php
+++
@@ -1,8 +0,0 @@
-<?php include_once('header.php') ?>
-<section class="hero" id="hero">
-    <div class="hero-text">
-    <h1>Services</h1>
-    <p>Will be updated soon.</p>
-    </div>
-</section>
-<?php include_once('footer.php') ?>
